dd091709b9b79b735086092f0ca22353c1140694
Sorcia <anna@MacBook-Air-Anna.local>
2016-07-16 11:07:51 +0300

    php added

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My test page</title>
+</head>
+<body>
+<style>
+    body {
+        font-family: "Abadi MT Condensed";
+    }
+    h1 {
+        color:coral;
+        font-size: 40px;
+    }
+
+
+    li {
+        color: darkmagenta;
+        font-weight: bolder;
+        text-decoration: line-through;
+        font-family: tahoma;
+
+    }
+    .green {
+        color: green;
+    }
+    .underline {
+        text-decoration: underline;
+    }
+    #myMusicHeader {
+        color: red;
+    }
+    p {
+        text-align: justify;
+    }
+    .s {
+        font-weight: bold;
+    }
+    ol li {
+        cursor: pointer;
+    }
+</style>
+<h1>My first page</h1>
+<p>this page about <strong> my <i>life</i></strong></p>
+<img src="http://vignette1.wikia.nocookie.net/warframe/images/5/51/%D0%A1%D0%BC%D0%B0%D0%B9%D0%BB.png/revision/latest?cb=20130627100903&path-prefix=ru">
+<img src="fl.jpg" height="50" width="50">
+<h2 id= "myMusicHeader" class="green">My music</h2>
+<p>my music <span class="s"> tuta Lorem</span> ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate delectus eaque eveniet ex explicabo facilis ipsa, iste magnam minima natus quidem quo quod recusandae, repellendus saepe sit unde voluptatem.</p>
+<ul>
+    <li>
+        <a href="http://google.com" target="_blank">My song 1</a>
+    </li>
+    <li class="green underline">My song 2</li>
+    <li>My song 3</li>
+</ul>
+<table border="1">
+    <tr>
+        <td>Cell 1</td>
+        <td>Cell 2</td>
+        <td>Cell 3</td>
+        <td>Cell 4</td>
+    </tr>
+    <tr>
+        <td>Cell 1</td>
+        <td>Cell 2</td>
+        <td>Cell 3</td>
+        <td>Cell 4</td>
+
+    </tr>
+</table>
+
+<iframe src="http://ukr.net" frameborder="0" width="1000" height="200"></iframe>
+</body>
+</html>
